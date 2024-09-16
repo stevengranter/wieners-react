badfceece42c968e217de42deb6a6df94baab167
Steven Granter <stevengranter@gmail.com>
2024-09-16 19:08:58 -0230

    :fire: Removed PixiJS helloworld from App, restored TitleScreen.tsx

--- src/components/TitleScreen.tsx
+++ src/components/TitleScreen.tsx
@@ -1,20 +1,15 @@
 // TitleScreen.tsx
 import logo from "../assets/images/ui/wfh-logo.webp";
-import { ReactElement, useState } from "react";
 
 import styles from "./TitleScreen.module.css";
-import CharacterPopup from "./CharacterPopup.tsx";
 function TitleScreen() {
-  const [gotoNext, setGotoNext] = useState<ReactElement | null>(null);
   function handleStart() {
     console.log("Starting game...");
-    setGotoNext(<CharacterPopup />);
   }
   return (
     <div className="canvas-overlay-container" id={styles["title-screen"]}>
       <img src={logo} alt="Wieners from Heaven Logo" />
       <StartButton handleStart={handleStart} />
-      {gotoNext}
     </div>
   );
 }
